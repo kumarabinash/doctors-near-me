e5647abd5b8f9ec4a45e14d9283c974060e4242e
Kumar Abinash <abinash.b2@gmail.com>
2014-03-21 19:20:56 +0530

    adding extra features.

--- app/view/Main.js
+++ app/view/Main.js
@@ -20,17 +20,17 @@ Ext.define('DoctorsNearMe.view.Main', {
                 items: {
                     docked: 'top',
                     xtype: 'titlebar',
-                    title: 'CityMap'
+                    title: 'DoctorsNearMe'
                 },
 
                 html: [
-                    "<h1>CityMap lets you see the map of your city and whats going on it your city on a map.</h1>",
-                    "<h2>Please click on the CityMap tab to search for your city</h2>",
-                    "Please allow CityMap to access your current location to show you the map of your current city."
+                    "<h2>DoctorsNearMe lets you find Doctors/Medical Facilites near your area.</h2>",
+                    "<h2>Please click on the 'Find' tab, to start search.</h2>",
+                    "Please allow DoctorsNearMe to access your current location to show you the map of your current city."
                 ].join("")
             },
             {
-                title: 'CityMap',
+                title: 'DoctorsNearMe',
                 iconCls: 'action',
                 xtype: 'map',
                 useCurrentLocation: true,
@@ -39,9 +39,80 @@ Ext.define('DoctorsNearMe.view.Main', {
                     zoom: 13,
                     mapTypeId: google.maps.MapTypeId.ROADMAP
                 },
-
                 items: [
                     {
+                        xtype: 'panel',
+                        border: false,
+                        flex: 5,
+                        layout: 'card',
+                        style: 'background-color: #fff',
+                        items: [
+                                {
+                                    title: 'Page 1',
+                                    html: '<h1>This is page 1</h1>'
+                                },
+                                {
+                                    title: 'Page 2',
+                                    html: '<h1>This is page 2</h1>'
+                                },
+                                {
+                                    title: 'Page 3',
+                                    html: '<h1>This is page 3</h1>'
+                                },
+                                {
+                                    title: 'Page 4',
+                                    html: '<h1>This is page 4</h1>'
+                                },
+                                {
+                                    title: 'Page 5',
+                                    html: '<h1>This is page 5</h1>'
+                                }
+                            ],
+                            fbar: [
+                            {
+                                id: 'btnPrev',
+                                text: 'Previous',
+                                handler: function(btn) {
+                                    btn.up('window').navigate('prev');
+                                }
+                            },
+                            {
+                                id: 'btnNext',
+                                text: 'Next',
+                                handler: function(btn) {
+                                    btn.up('window').navigate('next');
+                                }
+                            },
+                            {
+                                id: 'btnFinish',
+                                text: 'Finish',
+                                handler: function(btn) {
+                                    btn.up('window').close();
+                                }
+                            }
+                        ],
+
+                        navigate: function(to) {
+                            var dv = this.down('dataview'),
+                                layout = this.down('panel').getLayout(),
+                                items = layout.getLayoutItems(),
+                                idx;
+
+                            if (Ext.isNumber(to)) {
+                                layout.setActiveItem(to);
+                            } else {
+                                layout[to]();
+                            }
+
+                            this.down('#btnPrev').setDisabled(!layout.getPrev());
+                            this.down('#btnNext').setDisabled(!layout.getNext());
+                            this.down('#btnFinish').setDisabled(layout.getNext());
+
+                            idx = Ext.Array.indexOf(items, layout.getActiveItem());
+
+                            dv.select(idx);
+                        },
+                        {
                         docked: 'top',
                         xtype: 'toolbar',
                         title: 'Map of your city',
